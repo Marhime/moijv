a6225e659247bc5e232c3db9836e60473ac418a2
marhime <maxime.joyes@gmail.com>
2018-03-27 17:35:54 +0200

    Users added in view

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -10,11 +11,14 @@ class HomeController extends Controller
     /**
      * @Route("/home", name="home")
      */
-    public function index()
+    public function index(UserRepository $userRepository)
     {
-        $message = 'Enculé de ta race !';
+        // $userRepo est passé automatiquement en parametre par Symfony -> injection de dépendance. On n'a donc pas à l'instancier nous-même
+        // $userRepo effectuera ici un SELECT * FROM user ...
+        $userList = $userRepository->findAll();
+
         return $this->render("home.html.twig",[
-           'msg' => $message
+           'users' => $userList
         ]);
     }
 }
